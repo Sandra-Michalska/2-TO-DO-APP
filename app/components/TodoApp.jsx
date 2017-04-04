1d5eeda8a2ebcb4982e9dbdb11a074f259c56f08
Sandra <you@example.com>
2017-04-04 14:27:05 +0200

    Add timestamps

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@
 // get todos out of local storage and add new todos
 var React = require('react');
 var uuid = require('uuid');
+var moment = require('moment');
 
 var Add = require('Add');
 var Search = require('Search');
@@ -28,7 +29,8 @@ var TodoApp = React.createClass({
                 {
                     id: uuid(),
                     text: todoValue,
-                    completed: false
+                    completed: false,
+                    createdAt: moment().unix()
                 }
             ]
         });
@@ -43,6 +45,7 @@ var TodoApp = React.createClass({
         var updatedTodos = this.state.todos.map((todo) => {
             if (todo.id === id) {
                 todo.completed = !todo.completed;
+                todo.completedAt = todo.completed ? moment().unix() : undefined;
             }
             return todo;
         });
